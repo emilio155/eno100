c349d9a6fdf20bdb8cff20ec86d5234e6fb7c46b
emilio155 <emilio260002@hotmail.com>
2025-01-26 13:45:55 +0100

    Add files via upload

--- #eno100.py
+++ #eno100.py
@@ -7,3 +7,9 @@
 #The is all code :true 
 #vpn-server = my vpn-server = name = eno100 + pin = 260808@ 
 #anonym-surfing = ip-schutz + browser = all anonym + loogin = is all :true 
+#of wifi-adapter = name = 2.6.2.0 = d-link 192.168.178.1-40-86-CB-D2-24-AB:mac-adress:is:true 
+#Realtek RTL8192EU Wireless LAN 802.11n USB 2.0 Network Adapter is :true 
+#1030.44.1014.2024 is all:true=eno100.net16 = 236202333266 is all:true=login vpn data = ip-adress=192.168.178.1 host-name=eno100 kennwort=260808@ server-name=einfach-emilio.net DNS-adress=2.6.2.0#backend and backend =private key:363424 236206 online key:166166#
+#code is all =true=k2k2=all =code:true 
+#print(#true_allcode=k2k2#)
+#print("k2k2")
